904dc11f5391ac54267d18d67a3d77587f168bbe
gokceerdem <gokce.erdem@boun.edu.tr>
2016-05-02 12:44:24 +0300

    segment sec eklendi

--- train_svm.cpp
+++ train_svm.cpp
@@ -28,6 +28,7 @@ SegmentMoments SegMom;
 ImData myImData;
 time_t tstart, tend, mstart, mend;
 
+vector<int> lops, lops2;
 
 stringstream imgstream, mystream, seq_stream, imgstr, segmentstream, filterstream;
 
@@ -42,6 +43,8 @@ int labelNr, segment_no, label_no;
 
 int centerSum[2][1] = { 0 };
 int scalefactor; 
+
+bool flag;
 double sqrt6(double y)
 {
 	double x, z, tempf;
@@ -563,21 +566,22 @@ float sqrt5(const float m)
 
 float labels_array[250] = { 0 };
 float trainingData[250][numberofbins] = { 0 };
-float samples[100][numberofbins] = { 0 };
-
+float samples[1000][numberofbins] = { 0 };
+int imgandsegnr[1000][2]={0};
 getdataclass myData;
 
 void hesapla(int, int, int);
-void testyap(int, int);
+void testyap(int, int, int);
 
 void hesapla2(int, int, int);
 void testyap2(int, int);
 
+vector<int> segmentsec(vector<vector<Point>>);
 
 int index = 0 ;
 int sindex = 0;
-int train_sample_number, test_sample_number, train_sample_number2, test_sample_number2;
-
+int train_sample_number,  train_sample_number2, test_sample_number2, test_image_number;
+int test_sample_number = 0;
 Mat filt_crop;
 //int histSize = 3;    // bin size
 float range[] = { 0, 255 };
@@ -651,7 +655,7 @@ int main()
 		cout << "Number of test samples:" << endl;
 		cin >> test_sample_number;
 		do{
-			testyap(myData.testArr[sindex][0], myData.testArr[sindex][1]);
+			testyap(myData.testArr[sindex][0], myData.testArr[sindex][1],test_sample_number);
 			sindex = sindex + 1;
 
 		} while (sindex<test_sample_number);
@@ -727,16 +731,19 @@ int main()
 		params.term_crit = cvTermCriteria(CV_TERMCRIT_ITER, 100, 1e-6);
 
 		CvSVM SVM;
-		SVM.load("my_svm_2.xml");
+		SVM.load("my_svm.xml");
 		
+		
+		cout << "Number of test images:" << endl;
+		cin >> test_image_number;
+			test_sample_number = 0;
 
-		cout << "Number of test samples:" << endl;
-		cin >> test_sample_number;
-		do{
-			testyap(myData.testArr[sindex][0], myData.testArr[sindex][1]);
-			sindex = sindex + 1;
-
-		} while (sindex<test_sample_number);
+	
+		while (sindex<test_image_number){
+			
+					testyap(myData.testArr[sindex][0],1,test_sample_number);
+					sindex = sindex + 1;
+		}
 
 		Mat testing_data = Mat(test_sample_number, numberofbins, CV_32FC1, samples);
 		Mat testing_classifications = Mat(test_sample_number, 1, CV_32FC1);
@@ -1092,7 +1099,7 @@ void hesapla(int image_no, int segment_no, int label_no){
 		labelNr = 0;
 	}
 
-void testyap(int image_no, int segment_no){
+void testyap(int image_no, int segment_nos, int test_sample_number){
 		MatND hist;
 		int ivar = image_no;
 
@@ -1284,12 +1291,21 @@ void testyap(int image_no, int segment_no){
 			++nindx;
 		}
 
+			vector<int> pruneseg;
+				segmentsec(myImData.connComp);
+				pruneseg = lops;
+
+			if (pruneseg.empty() == false){	
+
+			for (int ls = 0; ls < lops.size() ; ls++)
+			
+{
+				segment_no =  pruneseg.back();
+				pruneseg.pop_back();
+
 		scalefactor = myImData.connComp.at(segment_no).size();
+
 		features(myImData, segment_no);
-		//	cout << mF.hueval << endl << mF.shaperatio << endl;
-
-		// Mat goster_filtre(myImData.h, myImData.w, CV_8UC1, Scalar(background));
-
 		vector<float> binvector;
 	
 	
@@ -1321,39 +1337,51 @@ void testyap(int image_no, int segment_no){
 			hist.release();
 		}
 
-				std::unordered_map<int, int> voting;
-			for (int d = 0; d<myImData.connComp.at(segment_no).size(); d++)
+			//	std::unordered_map<int, int> voting;
+			//for (int d = 0; d<myImData.connComp.at(segment_no).size(); d++)
 
-				{
-					Point component = myImData.connComp.at(segment_no).at(d);
-					++voting[I[component.y][component.x]];
-					
-				}
+			//	{
+			//		Point component = myImData.connComp.at(segment_no).at(d);
+			//		++voting[I[component.y][component.x]];
+			//		
+			//	}
 
-				int currentMax = 0;
-				int arg_max = 0;
-				for(auto it = voting.cbegin(); it != voting.cend(); ++it ) {
-					if (it ->second > currentMax) {
-						arg_max = it->first;
-						currentMax = it->second;
-					}
-				}
-				//cout << "Value " << arg_max << " occurs " << currentMax << " times " << endl;					
-				
-			float arg_max_f = arg_max/25;
+			//	int currentMax = 0;
+			//	int arg_max = 0;
+			//	for(auto it = voting.cbegin(); it != voting.cend(); ++it ) {
+			//		if (it ->second > currentMax) {
+			//			arg_max = it->first;
+			//			currentMax = it->second;
+			//		}
+			//	}
+			//	//cout << "Value " << arg_max << " occurs " << currentMax << " times " << endl;					
+			//	
+			//float arg_max_f = arg_max/26;
 				
 		binvector.push_back(mF.hueval);
 		binvector.push_back(mF.shaperatio);
 		binvector.push_back(mF.segmentLocation);
 		binvector.push_back(mF.r);
-		binvector.push_back(arg_max_f);
+		//binvector.push_back(arg_max_f);
 
 		for (int s = 0; s < numberofbins; s++){
-			samples[sindex][s] = binvector.at(s);
+			samples[test_sample_number][s] = binvector.at(s);
 		}
 
-	
-		voting.clear();
+			++test_sample_number;
+			
+		filt_crop.release();
+		binvector.clear();
+
+		mF.AverageFilterOutputs.clear();		
+		
+}
+		}
+		labelNr = 0;
+		segmentClrs.clear();
+		contourArray.clear();
+
+		myImData.connComp.clear();
 		nindx = 0;
 		nIndx.clear();
 		for (int ph = 0; ph<pyrHeight; ph++){
@@ -1372,25 +1400,14 @@ void testyap(int image_no, int segment_no){
 		}
 
 		for (int eqn = 0; eqn<MAX_EXP_NrOf_LABELS; eqn++){ EQ[eqn] = 0; }
-		
-		filt_crop.release();
-		binvector.clear();
-		segmentClrs.clear();
-		contourArray.clear();
-
-		mF.AverageFilterOutputs.clear();
-
 		myImData.filter.clear();
 		myImData.hsv_filter.clear();
-	
-		myImData.connComp.clear();
 		myImData.original.release();
 		myImData.intensity.release();
 		myImData.hsvImg.release();
-		labelNr = 0;
-
+		//if (pruneseg.empty() == true )
+		//	{return;}
 	}
-
 
 void hesapla2(int image_no, int segment_no, int label_no){
 		MatND hist;
@@ -2032,5 +2049,271 @@ void testyap2(int image_no, int segment_no){
 
 	}
 
+vector<int> segmentsec(vector<vector<Point>> connComp){
+	//	MatND hist;
+	lops.clear();
+	//	int segment_no;
+	//	int ivar = image_no;
 
+	std::unordered_map<int, int> voting;
+
+	//	mystream << imagename1 << std::setfill('0') << std::setw(4) << ivar << type;
+	//	string myfilename = mystream.str();
+	//	mystream.str("");
+	//	Mat src = imread(myfilename, CV_LOAD_IMAGE_COLOR);
+
+	//	Mat dst;
+	//	pyrDown(src, dst, Size(src.cols / 2, src.rows / 2));
+	//	bilateralFilter(dst, myImData.original, 9, 30, 30);
+
+	//	cvtColor(myImData.original, myImData.intensity, CV_BGR2GRAY);
+	//	cvtColor(myImData.original, myImData.hsvImg, CV_BGR2HSV);
+
+	//	myImData.h = myImData.original.rows;
+	//	myImData.w = myImData.original.cols;
+
+	//	vector<Mat> hsvchannels;
+	//	split(myImData.hsvImg, hsvchannels);
+
+	//	myImData.hsv_filter.push_back(hsvchannels[0]);
+	//	myImData.hsv_filter.push_back(hsvchannels[1]);
+	//	myImData.hsv_filter.push_back(hsvchannels[2]);
+
+	//	hsvchannels.clear();
+
+	//	//keep filters in here
+	//	vector<Mat> filters;
+
+	//	for (int f = 0; f<numberoffilters; f++)
+	//	{
+	//		filters.push_back(readFilter(f, 29));
+	//	}
+	//	for (int i = 0; i<numberoffilters; i++){
+	//		//filter original image and create filtered outputs
+	//		applyFilter(myImData, filters[i], i);
+
+	//		
+	//	}
+	//	myImData.filter.push_back(myImData.hsv_filter.at(0)); //Hue filter result added to 5 spatial filter result
+	//	filters.clear();
+
+	//	for (int k = 0; k < (myImData.h * myImData.w); k++){
+	//		int x = k % (myImData.w);
+	//		int y = (k - x) % (myImData.w - 1);
+	//		bool flag = false;
+
+	//		for (list<hsvc>::iterator it = col_hash_map.begin(); it != col_hash_map.end(); ++it){
+
+	//			int val_h = myImData.hsv_filter.at(0).at<uchar>(y, x);
+	//			int val_s = myImData.hsv_filter.at(1).at<uchar>(y, x);
+	//			int val_v = myImData.hsv_filter.at(2).at<uchar>(y, x);
+
+	//			if (val_h >= it->hlow && val_h <= it->hhigh && val_s >= it->slow &&
+	//				val_s <= it->shigh && val_v >= it->vlow && val_v <= it->vhigh){
+
+	//			I[y][x] = it->col_name;
+
+	//				flag = true; break;
+	//			}
+
+	//		}
+	//	}
+
+	//	Labeling(labelNr, label, I, Q, EQ, myImData);
+
+	//	LabelEqualization(EQ, label, myImData, labelColors);
+
+	//	/*Merge small components with their nearest component*/
+	//	std::unordered_map<int, int> occurrences;
+
+	//	for (int i = 0; i < myImData.h; ++i){
+	//		for (int j = 0; j < myImData.w; ++j){
+
+	//			++occurrences[label[i][j]];
+	//		}
+	//	}
+
+	//	for (int i = 0; i < myImData.h; ++i){
+	//		for (int j = 0; j < myImData.w; ++j){
+
+	//			if (occurrences[label[i][j]] < MAX_PxNr_SMALL_AREA) {
+	//				EQ[label[i][j]] = Q[i][j];
+	//				Q[i][j + 1] = Q[i][j];
+
+	//			}
+	//		}
+	//	}
+	//	occurrences.clear();
+
+	//	// LabelEqualization(EQ, label, myImData,labelColors);
+	//	vector<int> nIndx;
+	//	int indx = 1;
+
+	//	while (indx != labelNr + 1){
+	//		contourArray.clear();
+	//		for (int i = 0; i < myImData.h; i++) {
+	//			for (int j = 0; j < myImData.w; j++) {
+
+	//				int val = label[i][j];
+	//				if (val == indx){
+	//					contourArray.push_back(Point(j, i));
+	//				}
+	//			}
+	//		}
+	//		if (contourArray.empty() == false){
+
+	//			myImData.connComp.push_back(contourArray);
+	//			int clrv;
+
+	//			if (contourArray.size() == 1){
+	//				clrv = I[contourArray.at(0).y][contourArray.at(0).x];
+	//			}
+	//			if (contourArray.size()>1){
+	//				clrv = I[contourArray.at(contourArray.size() - 1).y][contourArray.at(contourArray.size() - 1).x];
+	//			}
+
+	//			segmentClrs.push_back(clrv);
+
+	//			if (contourArray.size()> MIN_PxNr_BIG_AREA){
+	//				nIndx.push_back(myImData.connComp.size() - 1);
+	//			}
+
+	//		}
+	//		++indx;
+	//	}
+
+	//	for (int nfc = 0; nfc< nIndx.size(); nfc++){
+
+	//		int numberofcomponents = nIndx.at(nfc);
+	//		Mat component_Img = Mat::zeros(myImData.h, myImData.w, CV_8UC1);
+	//		Mat dilated_component_Img, dst;
+	//		Mat eroded_;
+	//		// Create binary image of big segment
+	//		for (int comp = 0; comp < myImData.connComp.at(numberofcomponents).size(); comp++){
+	//			Point component = myImData.connComp.at(numberofcomponents).at(comp);
+	//			component_Img.at<uchar>(component.y, component.x) = 255;
+	//		}
+
+	//		dilate(component_Img, dilated_component_Img, dilation_element);
+
+	//		// Obtain adjacent parts
+	//		cv::bitwise_xor(component_Img, dilated_component_Img, dst);
+	//
+	//		vector<Point> nonZeroCoordinates;		//keep adjacent pixels in here
+	//		findNonZero(dst, nonZeroCoordinates);
+
+	//		int	ColorNr1 = segmentClrs.at(numberofcomponents);
+	//		int ColorNr2;
+	//		int newLabel = label[myImData.connComp.at(numberofcomponents).at(0).y][myImData.connComp.at(numberofcomponents).at(0).x];
+
+	//		for (int g = 0; g<nonZeroCoordinates.size(); g++){
+
+	//			Point AdjPoint = nonZeroCoordinates.at(g);
+	//			ColorNr2 = I[AdjPoint.y][AdjPoint.x];
+	//			if (relationLUT[ColorNr1][ColorNr2] == 1){
+
+	//				EQ[label[AdjPoint.y][AdjPoint.x]] = newLabel;
+
+	//			}
+
+	//		}
+
+	//	}
+
+	//	LabelEqualization(EQ, label, myImData, labelColors);
+
+	//	myImData.connComp.clear();
+
+	//	int nindx = 1;
+	//	while (nindx != labelNr + 1){
+
+	//		contourArray.clear();
+	//		for (int i = 0; i < myImData.h; i++) {
+	//			for (int j = 0; j < myImData.w; j++) {
+
+	//				int nval = label[i][j];
+	//				if (nval == nindx){
+
+	//					contourArray.push_back(Point(j, i));
+
+	//				}
+	//			}
+	//		}
+	//		if (contourArray.empty() == false){
+	//			myImData.connComp.push_back(contourArray);
+	//		}
+	//		++nindx;
+	//	}
+
+
+		for (int tSegmentNumber=0; tSegmentNumber < connComp.size(); tSegmentNumber++)
+		{
+			if (connComp.at(tSegmentNumber).size() > 170){
+				
+				for (int d = 0; d<connComp.at(tSegmentNumber).size(); d++)
+
+				{
+					Point component = connComp.at(tSegmentNumber).at(d);
+					++voting[I[component.y][component.x]];
+					
+				}
+					int currentMax = 0;
+				int arg_max = 0;
+				for(auto it = voting.cbegin(); it != voting.cend(); ++it ) {
+					if (it ->second > currentMax) {
+						arg_max = it->first;
+						currentMax = it->second;
+					}
+				}
+
+			if(arg_max == 8 || arg_max == 9 || arg_max == 18 || arg_max == 19 || arg_max == 22 || arg_max ==23 || arg_max == 24 || arg_max == 25){
+				
+					lops.push_back(tSegmentNumber);
+			  }
+
+				voting.clear();
+			}
+		}
+		
+		//	nindx = 0;
+		//nIndx.clear();
+		//for (int ph = 0; ph<pyrHeight; ph++){
+		//	for (int pw = 0; pw<pyrWidth; pw++){
+		//		label[ph][pw] = 0;
+		//		I[ph][pw] = 0;
+
+		//	}
+		//}
+		//for (int ph = 0; ph<pyrHeight; ph++){
+		//	for (int pw = 0; pw<pyrWidth + 1; pw++){
+
+		//		Q[ph][pw] = 0;
+
+		//	}
+		//}
+
+	//	for (int eqn = 0; eqn<MAX_EXP_NrOf_LABELS; eqn++){ EQ[eqn] = 0; }
+
+	//	filt_crop.release();
+	////	binvector.clear();
+	//	segmentClrs.clear();
+	//	contourArray.clear();
+
+	//	myImData.filter.clear();
+	//	myImData.hsv_filter.clear();
+
+	//	myImData.connComp.clear();
+	//	myImData.original.release();
+	//	myImData.intensity.release();
+	//	myImData.hsvImg.release();
+	//	labelNr = 0;
+
+		connComp.clear();
+			return	lops;
+
+		}
+		
+
+	
+		
 
